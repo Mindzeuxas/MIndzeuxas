1f567e3f8e24d760009360b6c867441f861d86bd
mindaugas.samsonas <mindaugas.samsonas@studentas.bit.lt>
2025-02-05 13:29:13 +0200

    numeriu randomise 1

--- js/variables/06-numbers.js
+++ js/variables/06-numbers.js
@@ -3,3 +3,16 @@ console.log(2 ** 5); // dviguba zvaigzdute kelia laipsniu
 console.log(Math.PI.toFixed(50));
 //arr[i] * Math.pow(10, i)
 //arr[i] * (10 ** i);
+
+
+/**
+ * returns integer in interval
+ * @param {number} min - min value in interval
+ * @param {number} max  - max value in interval
+ * @returns {number} - returns integer in interval
+ */
+function randomize(min, max)
+{
+    return Math.floor(Math.random * (max - min + 1)) + min;
+
+}
