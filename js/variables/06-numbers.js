7ff6a229294953d81bf2db28ef48515a15a999e7
mindaugas.samsonas <mindaugas.samsonas@studentas.bit.lt>
2025-02-07 03:16:09 +0200

    NAUJAI

--- js/variables/06-numbers.js
+++ js/variables/06-numbers.js
@@ -5,8 +5,8 @@ console.log(Math.PI.toFixed(50));
 //arr[i] * (10 ** i);
 
 
-/**
- * returns integer in interval
+/** dokumentacija
+ * returns integer in interval with both sides
  * @param {number} min - min value in interval
  * @param {number} max  - max value in interval
  * @returns {number} - returns integer in interval
@@ -15,4 +15,34 @@ function randomize(min, max)
 {
     return Math.floor(Math.random * (max - min + 1)) + min;
 
+}
+//tas +1 reikalingas, kad ir galine reiksme paimtu
+function apvalinimas(a, amount)
+{
+    return Math.round(a * (10 ** amount)) / (10 ** amount);
+}
+
+console.log(getSum(16));
+function getSum(num)
+{
+    let suma = 0;
+    while(num / 10 > 0)
+    {
+        suma += ((num % 10));
+        num = Math.floor(num  / 10);
+    }
+
+    return suma;
+}
+console.log(getArray(5));
+
+function getArray(number)
+{
+    let arr = [];
+    for (let i = 1; i <= 10; i++)
+    {
+        arr[i - 1] = number * i;
+        //arr.push(number * i);
+    }
+    return arr;
 }
